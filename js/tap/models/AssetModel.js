9a793fe6820a38cdabd0808050a3aaf8c9a05858
Daniel Cervantes <dlcerva@gmail.com>
2013-02-10 23:05:13 -0500

    Updated models to RequireJS ready modules.

--- js/tap/models/AssetModel.js
+++ js/tap/models/AssetModel.js
@@ -1,88 +1,92 @@
-// TapAPI Namespace Initialization //
-if (typeof TapAPI === 'undefined'){TapAPI = {};}
-if (typeof TapAPI.models === 'undefined'){TapAPI.models = {};}
-// TapAPI Namespace Initialization //
-
-// define asset model
-TapAPI.models.Asset = Backbone.Model.extend({
-	parse: function(response) {
-		response.propertySet = new TapAPI.collections.PropertySet(
-			response.propertySet,
-			{id: response.id}
-		);
-
-		if (response.source) {
-			response.source = new TapAPI.collections.Sources(
-				response.source,
-				{
-					id: response.id,
-					asset: this
-				}
+define([
+	'underscore',
+	'backbone',
+	'tap/app',
+	'tap/collections/PropertySetCollection',
+	'tap/collections/SourceCollection',
+	'tap/collections/ContentCollection'
+], function(_, Backbone, App, PropertySetCollection, SourceCollection, ContentCollection) {
+	var assetModel = Backbone.Model.extend({
+		parse: function(response) {
+			response.propertySet = new PropertySetCollection(
+				response.propertySet,
+				{id: response.id}
 			);
-		}
 
-		if (response.content) {
-			response.content = new TapAPI.collections.Content(
-				response.content,
-				{
-					id: response.id,
-					asset: this
-				}
-			);
-		}
+			if (response.source) {
+				response.source = new SourceCollection(
+					response.source,
+					{
+						id: response.id,
+						asset: this
+					}
+				);
+			}
 
-		return response;
-	},
-	getSourcesByPart: function(part) {
-		if (_.isUndefined(this.get('source'))) return undefined;
+			if (response.content) {
+				response.content = new ContentCollection(
+					response.content,
+					{
+						id: response.id,
+						asset: this
+					}
+				);
+			}
 
-		var sources, models;
-		sources = this.get('source').where({"part": part, "lang": tap.language});
-		if (sources.length === 0) {
-			sources = this.get('source').where({"part": part});
-		}
-		if (sources.length) {
-			models = sources;
-		}
-		return models;
-	},
-	getContentsByPart: function(part) {
-		if (_.isUndefined(this.get('content'))) return undefined;
+			return response;
+		},
+		getSourcesByPart: function(part) {
+			if (_.isUndefined(this.get('source'))) return undefined;
 
-		var contents, models;
-		contents = this.get('content').where({"part": part, "lang": tap.language});
-		if (contents.length === 0) {
-			contents = this.get('content').where({"part": part});
-		}
-		if (contents.length) {
-			models = contents;
-		}
-		return models;
-	},
-	getSourcesByFormat: function(format) {
-		if (_.isUndefined(this.get('source'))) return undefined;
+			var sources, models;
+			sources = this.get('source').where({"part": part, "lang": App.tap.language});
+			if (sources.length === 0) {
+				sources = this.get('source').where({"part": part});
+			}
+			if (sources.length) {
+				models = sources;
+			}
+			return models;
+		},
+		getContentsByPart: function(part) {
+			if (_.isUndefined(this.get('content'))) return undefined;
 
-		var sources, models;
-		sources = this.get('source').where({"format": format, "lang": tap.language});
-		if (sources.length === 0) {
-			sources = this.get('source').where({"format": format});
-		}
-		if (sources.length) {
-			models = sources;
-		}
-		return models;
-	},
-	getContentsByFormat: function(format) {
-		if (_.isUndefined(this.get('content'))) return undefined;
+			var contents, models;
+			contents = this.get('content').where({"part": part, "lang": App.tap.language});
+			if (contents.length === 0) {
+				contents = this.get('content').where({"part": part});
+			}
+			if (contents.length) {
+				models = contents;
+			}
+			return models;
+		},
+		getSourcesByFormat: function(format) {
+			if (_.isUndefined(this.get('source'))) return undefined;
 
-		var contents, models;
-		contents = this.get('content').where({"format": format, "lang": tap.language});
-		if (contents.length === 0) {
-			contents = this.get('content').where({"format": format});
+			var sources, models;
+			sources = this.get('source').where({"format": format, "lang": App.tap.language});
+			if (sources.length === 0) {
+				sources = this.get('source').where({"format": format});
+			}
+			if (sources.length) {
+				models = sources;
+			}
+			return models;
+		},
+		getContentsByFormat: function(format) {
+			if (_.isUndefined(this.get('content'))) return undefined;
+
+			var contents, models;
+			contents = this.get('content').where({"format": format, "lang": App.tap.language});
+			if (contents.length === 0) {
+				contents = this.get('content').where({"format": format});
+			}
+			if (contents.length) {
+				models = contents;
+			}
+			return models;
 		}
-		if (contents.length) {
-			models = contents;
-		}
-		return models;
-	}
+	});
+	return assetModel;
 });
--- js/tap/models/TourModel.js
+++ js/tap/models/TourModel.js
@@ -1,50 +1,52 @@
-// TapAPI Namespace Initialization //
-if (typeof TapAPI === 'undefined'){TapAPI = {};}
-if (typeof TapAPI.models === 'undefined'){TapAPI.models = {};}
-// TapAPI Namespace Initialization //
+define([
+	'underscore',
+	'backbone',
+	'tap/app',
+	'tap/collections/PropertySetCollection'
+], function(_, Backbone, App, PropertySetCollection) {
+	var tourModel = Backbone.Model.extend({
+		get: function(attr) { // override get method
+			if(!this.attributes[attr]) return this.attributes[attr];
+			switch(attr) {  // retrieve attribute based on language
+				case 'description':
+				case 'title':
+					if (this.attributes[attr].length === 0) return undefined;
 
-// define tour model
-TapAPI.models.Tour = Backbone.Model.extend({
-	get: function(attr) { // override get method
-		if(!this.attributes[attr]) return this.attributes[attr];
-		switch(attr) {  // retrieve attribute based on language
-			case 'description':
-			case 'title':
-				if (this.attributes[attr].length === 0) return undefined;
+					var value, property;
 
-				var value, property;
-
-				property = _.find(this.attributes[attr], function(item) {
-					return item.lang === tap.language;
-				});
-
-				if (!property && tap.language !== tap.defaultLanguage) {
 					property = _.find(this.attributes[attr], function(item) {
-						return item.lang === tap.defaultLanguage;
+						return item.lang === App.tap.language;
 					});
-				}
 
-				if (!property) {
-					property = _.find(this.attributes[attr], function(item) {
-						return item.lang === undefined || item.lang === "";
-					});
-				}
+					if (!property && App.tap.language !== App.tap.defaultLanguage) {
+						property = _.find(this.attributes[attr], function(item) {
+							return item.lang === App.tap.defaultLanguage;
+						});
+					}
 
-				if (property) {
-					value = property.value;
-				}
-				
-				return value;
-			default:
-				return this.attributes[attr];
+					if (!property) {
+						property = _.find(this.attributes[attr], function(item) {
+							return item.lang === undefined || item.lang === "";
+						});
+					}
+
+					if (property) {
+						value = property.value;
+					}
+
+					return value;
+				default:
+					return this.attributes[attr];
+			}
+		},
+		parse: function(response) {
+			response.propertySet = new PropertySetCollection(
+				response.propertySet,
+				{id: response.id}
+			);
+
+			return response;
 		}
-	},
-	parse: function(response) {
-		response.propertySet = new TapAPI.collections.PropertySet(
-			response.propertySet,
-			{id: response.id}
-		);
-
-		return response;
-	}
+	});
+	return tourModel;
 });
