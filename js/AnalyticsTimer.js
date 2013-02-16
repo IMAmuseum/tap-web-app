512679516fb453ad3a854992687d3a9994bc5579
Daniel Cervantes <dlcerva@gmail.com>
2013-02-16 10:53:23 -0500

    Updates to audio. Updated analytics timer.

--- js/AnalyticsTimer.js
+++ js/AnalyticsTimer.js
@@ -1,45 +1,45 @@
 define([
-    'jquery',
-    'underscore',
     'tap/TapAPI'
-], function($, _, TapAPI) {
-    var analyticsTimer = {
-        initialize: function(category, variable, opt_label) {
+], function(TapAPI) {
+        function AnalyticsTimer(category, variable, optLabel) {
             this.category = category;
             this.variable = variable;
-            this.label = opt_label ? opt_label : undefined;
-            this.start_time = null;
+            this.label = optLabel ? optLabel : undefined;
+            this.startTime = null;
             this.elapsed = 0;
-            this.min_threshold = null;
-            this.max_threshold = null;
-            this.min_clamp = false;
-            this.max_clamp = true;
+            this.minThreshold = null;
+            this.maxThreshold = null;
+            this.minClamp = false;
+            this.maxClamp = true;
+        }
+
+        AnalyticsTimer.prototype.start = function() {
+            this.startTime = new Date().getTime();
             return this;
-        },
-        start: function() {
-            this.start_time = new Date().getTime();
-            return this;
-        },
-        stop: function() {
-            if (this.start_time !== null) {
-              this.elapsed = this.elapsed + (new Date().getTime()) - this.start_time;
-              this.start_time = null;
+        };
+
+        AnalyticsTimer.prototype.stop = function() {
+            if (this.startTime !== null) {
+              this.elapsed = this.elapsed + (new Date().getTime()) - this.startTime;
+              this.startTime = null;
             }
             return this;
-        },
-        reset: function() {
+        };
+
+        AnalyticsTimer.prototype.reset = function() {
             this.elapsed = 0;
             return this;
-        },
-        send: function() {
+        };
+
+        AnalyticsTImer.prototype.send = function() {
             this.stop(); // update the timer
 
             // If threshold criteria are not met, do not send
-            if (((this.min_threshold === null) || this.min_clamp || (this.elapsed >= this.min_threshold)) &&
-                ((this.max_threshold === null) || this.max_clamp || (this.elapsed <= this.max_threshold))) {
+            if (((this.minThreshold === null) || this.minClamp || (this.elapsed >= this.minThreshold)) &&
+                ((this.maxThreshold === null) || this.maxClamp || (this.elapsed <= this.maxThreshold))) {
                 // At this point, we should clamp
-                if ((this.min_threshold !== null) && (this.elapsed < this.min_threshold)) this.elapsed = this.min_threshold;
-                if ((this.max_threshold !== null) && (this.elapsed > this.max_threshold)) this.elapsed = this.max_threshold;
+                if ((this.minThreshold !== null) && (this.elapsed < this.minThreshold)) this.elapsed = this.minThreshold;
+                if ((this.maxThreshold !== null) && (this.elapsed > this.maxThreshold)) this.elapsed = this.maxThreshold;
 
                 TapAPI.gaq.push(['_trackTiming', this.category, this.variable, this.elapsed, this.label]);
 
@@ -48,7 +48,7 @@ define([
             this.start(); // keep the timer running
 
             return this;
-        }
-    };
-    return analyticsTimer;
+        };
+
+    return AnalyticsTimer;
 });
