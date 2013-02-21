e21a68e42e53b02f4d554df810cded1a099efbf4
Daniel Cervantes <dlcerva@gmail.com>
2013-02-20 20:36:25 -0500

    Content view now appends the current views class to its element. Updated styling on keypad view.

--- js/tap/views/ContentView.js
+++ js/tap/views/ContentView.js
@@ -2,26 +2,30 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'tap/TapAPI',
     'tap/views/BaseView',
     'tap/TemplateManager'
-], function($, _, Backbone, TapAPI, BaseView, TemplateManager) {
+], function($, _, Backbone, BaseView, TemplateManager) {
     var contentView = BaseView.extend({
         id: 'content-wrapper',
         attributes: {
             'data-role': 'content'
         },
         initialize: function() {
+            this.currentView = undefined;
             this.listenTo(Backbone, 'tap.router.routed', this.render);
         },
         render: function(view) {
             if (view === undefined) return this;
 
-            if (TapAPI.currentView !== undefined) {
-                TapAPI.currentView.close();
+            // cleanup previous view
+            if (this.currentView !== undefined) {
+                this.$el.removeClass(this.currentView.id);
+                this.currentView.close();
             }
-            TapAPI.currentView = view;
-
+            // set current view
+            this.currentView = view;
+            // add the new view
+            this.$el.addClass(view.id);
             this.$el.html(view.render().$el);
             view.finishedAddingContent();
 
