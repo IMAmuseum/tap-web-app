999970bf753e7d42a501fac6efe75e857f0a117f
Daniel Cervantes <dlcerva@gmail.com>
2013-02-24 15:09:03 -0500

    Added header image.

--- js/tap/views/StopGroupView.js
+++ js/tap/views/StopGroupView.js
@@ -6,34 +6,43 @@ define([
     'tap/views/BaseView',
     'tap/TemplateManager'
 ], function($, _, Backbone, TapAPI, BaseView, TemplateManager) {
-	stopGroupView = BaseView.extend({
-		template: TemplateManager.get('stop-group'),
-		initialize: function() {
-			this._super('initialize');
-			this.title = this.model.get('title');
-		},
-		render: function() {
-			var stops = [];
-			_.each(this.model.get('connection'), function(connection) {
-				var stop = TapAPI.tourStops.get(connection.destId);
-				if (stop) {
-					stops.push({
-						id: stop.get('id'),
-						title: stop.get('title'),
-						icon: TapAPI.viewRegistry[stop.get('view')].icon
-					});
-				}
-			});
+    stopGroupView = BaseView.extend({
+        id: 'stop-group',
+        template: TemplateManager.get('stop-group'),
+        initialize: function() {
+            this._super('initialize');
+            this.title = this.model.get('title');
+        },
+        render: function() {
+            var stops = [],
+                header;
+            var description = this.model.get('description');
 
-			var description = this.model.get('description');
-			this.$el.html(this.template({
-				tourID: TapAPI.currentTour,
-				description: _.isEmpty(description) ? '' : description,
-				stops: stops
-			}));
+            var headerAsset = this.model.getAssetsByUsage('header_image');
+            if (!_.isUndefined(headerAsset)) {
+                header = headerAsset[0].get('source').at(0).get('uri');
+            }
 
-			return this;
-		}
-	});
-	return stopGroupView;
+            _.each(this.model.get('connection'), function(connection) {
+                var stop = TapAPI.tourStops.get(connection.destId);
+                if (stop) {
+                    stops.push({
+                        id: stop.get('id'),
+                        title: stop.get('title'),
+                        icon: TapAPI.viewRegistry[stop.get('view')].icon
+                    });
+                }
+            });
+
+            this.$el.html(this.template({
+                header: header,
+                tourID: TapAPI.currentTour,
+                description: _.isEmpty(description) ? '' : description,
+                stops: stops
+            }));
+
+            return this;
+        }
+    });
+    return stopGroupView;
 });
