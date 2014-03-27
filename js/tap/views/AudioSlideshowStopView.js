70852aa45e03174072b4d606cea4fe0a8db98695
Kyle Jaebker <kjaebker@gmail.com>
2014-03-27 18:49:36 -0400

    slider in stop group, and audio

--- js/tap/views/AudioSlideshowStopView.js
+++ js/tap/views/AudioSlideshowStopView.js
@@ -76,13 +76,21 @@ TapAPI.classes.views.AudioSlideshowStopView = TapAPI.classes.views.BaseView.exte
             sources.push('<source src="' + source.get('uri') + '" type="' + source.get('format') + '" />');
         });
 
+        // Find the transcription if one exists
+        var transcription = '';
+        var transcriptAsset = this.model.getAssetsByUsage('transcription');
+        if (!_.isEmpty(transcriptAsset)) {
+            transcription = transcriptAsset[0].get('content').at(0).get('data');
+        }
+
         // Render from the template
         this.$el.html(this.template({
             title: this.model.get('title'),
             imageSources: imageSources,
             sources: sources,
             description: description,
-            nextStopPath: this.getNextStopPath()
+            nextStopPath: this.getNextStopPath(),
+            transcription: transcription,
         }));
 
         return this;
@@ -194,11 +202,11 @@ TapAPI.classes.views.AudioSlideshowStopView = TapAPI.classes.views.BaseView.exte
         //     TapAPI.tracker.trackEvent('AudioStop', 'media_ended', label, timer.elapsed);
         // });
 
-        // // Add expand handler on the transcription toggle button
-        // this.$el.find('#transcription').on('expand', function(e, ui) {
-        //     var label = _.isObject(TapAPI.currentStop) ? TapAPI.currentStop.get("title") : null;
-        //     TapAPI.tracker.trackEvent('AudioStop', 'show_transcription', label, null);
-        // });
+        // Add expand handler on the transcription toggle button
+        this.$el.find('#transcription').on('expand', function(e, ui) {
+            var label = _.isObject(TapAPI.currentStop) ? TapAPI.currentStop.get("title") : null;
+            TapAPI.tracker.trackEvent('AudioStop', 'show_transcription', label, null);
+        });
 
 
         // this.player.play();
